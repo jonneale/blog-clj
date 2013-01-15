54f5112ef8e1ac3bdb3c21a729bad01dba1bce70
Jon Neale <jon.neale@uswitch.com>
2013-01-15 15:51:42 +0000

    Added dependencies to project and added app route

--- src/blog_clj/core.clj
+++ src/blog_clj/core.clj
@@ -1,6 +1,7 @@
-(ns blog-clj.core)
+(ns blog-clj.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
